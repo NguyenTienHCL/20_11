7a56c90d153bf6b7c2432b6a2d33692b28ef0176
NguyenTienHCL <nguyentaitien2610@gmail.com>
2022-11-20 20:26:00 +0700

    update

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Em';
+const nameGirl = 'Meii';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = "Happy Teacher's Day 20-11 My Bae ❤";
+const titleCard = 'Message';
+const contentCard = 'Chúc ny anh luôn tràn ngập niềm vui và thành công hơn trong sự nghiệp :3 mãi là cô giáo Vân dangyewxinkdep... đôi khi chảnh chóa tí cho lũ giòi con nó phải khiếp sợ hihi. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'Bae.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
