4798a2003ef32ee355d5dd5e6d3caed9a38a7eab
NguyenTienHCL <101309897+NguyenTienHCL@users.noreply.github.com>
2023-02-14 11:37:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,8 @@
 const nameGirl = '';
 const giftUrl = 'http://nodemy.vn';
-const eventName = "Happy Teacher's Day 20-11 My Bae ❤";
+const eventName = "Happy Valentine's Day ❤";
 const titleCard = 'Message';
-const contentCard = 'Chúc ny anh luôn tràn ngập niềm vui và thành công hơn trong sự nghiệp :3 mãi là cô giáo Vân dangyewxinkdep... đôi khi chảnh chóa tí cho lũ giòi con nó phải khiếp sợ hihi. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
-
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'Bae.jpg';
+const contentCard = 'Haluuu Sóc... Thật ra thì... anh cũm muốn nói lâu òy nhưng mà do hnay Valentine nên anh muốn tạo 1 tí xíuuu pấc ngờ cho Sóc ihihi :3 SÓC LÀM NY ANH NHẺ ❤';
+const giftImage = 'Squirrel.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
